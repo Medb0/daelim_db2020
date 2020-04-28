98af8ec2da890af180635c98b7f2a2418896005a
Medb0 <bxcv1230@gmail.com>
2020-04-28 16:51:05 +0900

    autoload

---
+++ modules/Medb/hello.php
@@ -0,0 +1,11 @@
+<?php
+namespace Modules\Medb;
+
+// 클래스 선언
+class Hello
+{
+    public function greeting()
+    {
+        echo "Hello World";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,7 +1,9 @@
 <?php
-echo "안녕하세요.";
 
-include_once "../hello.php";
-include_once "../hello.php";
+echo "안녕하세요";
 
-greeting();
+// include_once "../hello.php";
+include "../vendor/autoload.php";
+
+$obj = new \Modules\Medb\Hello;
+$obj->greeting();
